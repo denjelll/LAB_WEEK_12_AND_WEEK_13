9514bdb206a1d98304b4d52e64b30a4603a9d226
denjelll <165755569+denjelll@users.noreply.github.com>
2025-12-03 03:13:27 +0700

    Commit No. 3 – update to WorkManager

--- app/src/main/java/com/example/test_lab_week_13/MovieApplication.kt
+++ app/src/main/java/com/example/test_lab_week_13/MovieApplication.kt
@@ -1,10 +1,15 @@
 package com.example.test_lab_week_13
 
 import android.app.Application
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
 import com.example.test_lab_week_13.api.MovieService
 import com.example.test_lab_week_13.database.MovieDatabase
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 class MovieApplication : Application() {
     lateinit var movieRepository: MovieRepository
@@ -28,5 +33,24 @@ class MovieApplication : Application() {
         // create a MovieRepository instance
         movieRepository =
             MovieRepository(movieService, movieDatabase)
+
+        val constraints = Constraints.Builder()
+            // only run the task if the device is connected to the internet
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        // create a WorkRequest instance
+        // this will be used to schedule a background task
+        val workRequest = PeriodicWorkRequest
+            // run the task every 1 hour
+            // even if the app is closed or the device is restarted
+            .Builder(
+                MovieWorker::class.java, 1,
+                TimeUnit.HOURS
+            ).setConstraints(constraints)
+            .addTag("movie-work").build()
+        // schedule the background task
+        WorkManager.getInstance(
+            applicationContext
+        ).enqueue(workRequest)
     }
 }
--- app/src/main/java/com/example/test_lab_week_13/MovieRepository.kt
+++ app/src/main/java/com/example/test_lab_week_13/MovieRepository.kt
@@ -1,5 +1,6 @@
 package com.example.test_lab_week_13
 
+import android.util.Log
 import com.example.test_lab_week_13.api.MovieService
 import com.example.test_lab_week_13.database.MovieDao
 import com.example.test_lab_week_13.database.MovieDatabase
@@ -39,4 +40,17 @@ class MovieRepository(
         }.flowOn(Dispatchers.IO)
     }
 
+    suspend fun fetchMoviesFromNetwork() {
+        val movieDao: MovieDao = movieDatabase.movieDao()
+        try {
+            val popularMovies = movieService.getPopularMovies(apiKey)
+            val moviesFetched = popularMovies.results
+            movieDao.addMovies(moviesFetched)
+        } catch (exception: Exception) {
+            Log.d(
+                "MovieRepository",
+                "An error occurred: ${exception.message}"
+            )
+        }
+    }
 }
